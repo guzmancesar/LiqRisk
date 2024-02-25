c2dfcfad1267125019fb65f41118c0118792852d
Cesar`Guzman <cesar@cesarguzmansmbp.myfiosgateway.com>
2024-02-25 12:14:09 -0800

    testing normalization function

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import pandas 
 
 def main():
-	print("test")
+	source_df = source_ingestion()
+	source_df["spread_avg"] = normalize_column(source_df['spread_avg'])
+	print(source_df)
 
 
 
 
 
-def normalize_source():
+def source_ingestion():
 
 
 	source_volatility = pandas.read_csv('btc_volatility.csv')
@@ -19,7 +21,7 @@ def normalize_source():
 
 	#print(list(source_volume.columns))
 
-
+	#https://developers.google.com/machine-learning/data-prep/transform/normalization
 	data_to_normalize = pandas.merge(data_to_normalize, source_volume[["Time", "total"]], on='Time')
 	data_to_normalize = pandas.merge(data_to_normalize, source_spread[["Time", "avg"]], on='Time')
 
@@ -29,7 +31,22 @@ def normalize_source():
 
 
 	data_to_normalize.to_csv('output.csv')
-
+	return data_to_normalize
 	
 
-normalize_source()
+
+def normalize_column(column):
+    
+    mean = column.mean()
+    std_dev = column.std()
+    normalized_column = (column - mean) / std_dev
+    return normalized_column
+
+
+
+#def normalize_source_data(source_dataframe):
+
+main()
+
+
+
